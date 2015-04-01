9a6f02851ae948b20e1097e8642a4eb6bede4cce
sgolodychenko <golodychenko@gmail.com>
2015-04-01 19:07:27 +0300

    integration tests added

---
+++ Rank.Services.Integration.Tests/IntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rank.Services.Integration.Tests
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        [Test]
+        public void ConnectivityTest()
+        {
+            string page = "http://localhost:85";
+
+            var client = new HttpClient();
+            var response = client.GetAsync(page).Result;
+            var content = response.Content.ReadAsStringAsync();
+            Console.WriteLine(content.Status);
+            Console.WriteLine(content.Result);
+        }
+        
+    }
+}
